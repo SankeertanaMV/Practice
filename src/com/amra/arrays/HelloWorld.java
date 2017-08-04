cbbb6824576f27c8bcf2dbdd9965bb1d24bc0646
SankeertanaMV <sankeerthana.mv@gmail.com>
2017-08-04 17:12:35 -0400

    hello world pgm

---
+++ src/com/amra/arrays/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.amra.arrays;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		String[] s = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+		for (int i = 0 ; i<s.length; i++)
+			System.out.println(s[i]);
+
+	}
+
+}
